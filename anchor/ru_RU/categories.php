0f639388812a85f87fddb1ec2c09890f3b42ef93
artlancer <35227811+artlancer@users.noreply.github.com>
2018-01-15 21:02:01 +0500

    Create categories.php

---
+++ anchor/ru_RU/categories.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+    'category' => 'Категория',
+    'categories' => 'Категории',
+
+    'create_category' => 'Создать категорию',
+    'edit_category' => 'Редактировать &ldquo;%s&rdquo;',
+
+    // form fields
+    'title' => 'Название',
+    'title_explain' => 'Название категории.',
+    'title_missing' => 'Пожалуйста, введите название, минимум 3 символа.',
+
+    'slug' => 'ЧПУ',
+    'slug_explain' => 'ЧПУ для категории.',
+
+    'description' => 'Описание',
+    'description_explain' => 'О чем эта категория.',
+
+    // messages
+    'created' => 'Категория добавлена.',
+    'updated' => 'Категория обновлена.',
+    'deleted' => 'Категория удалена.',
+    'delete_error' => 'У Вас должна быть хотя бы одна категория.',
+
+);
