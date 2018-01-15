4c1c0ca5f35a2ab5d5ab8c5379d41f5d9524c9a4
artlancer <35227811+artlancer@users.noreply.github.com>
2018-01-15 21:13:18 +0500

    Create posts.php

---
+++ anchor/ru_RU/posts.php
@@ -0,0 +1,51 @@
+<?php
+
+return array(
+
+    'posts' => 'Посты',
+
+    'create_post' => 'Создать пост',
+    'noposts_desc' => 'У Вас нет ни одного поста!',
+
+    // form fields
+    'title' => 'Заголовок поста',
+    'title_explain' => '',
+    'title_missing' => 'Заголовок должен содержать минимум 3 символа',
+
+    'content' => 'Контент',
+    'content_explain' => 'Просто пишите.',
+
+    'slug' => 'ЧПУ и ID(Slug)',
+    'slug_explain' => 'Slug uri нужен для идентификации Вашго поста, может содержать только ascii символы',
+    'slug_missing' => 'Slug должен содержать минимум 3 символа, используйте только ascii символы',
+    'slug_duplicate' => 'Такой Slug уже существует',
+    'slug_invalid' => 'Slug должен содержать буквы',
+
+    'time' => 'Опубликовано (GMT)',
+    'time_explain' => 'Шаблон: YYYY-MM-DD HH:MM:SS',
+    'time_invalid' => 'Недопустимый шаблон времени',
+
+    'description' => 'Описание',
+    'description_explain' => '',
+
+    'status' => 'Статус',
+    'status_explain' => '',
+
+    'category' => 'Категория',
+    'category_explain' => '',
+
+    'allow_comments' => 'Разрешить комментарии',
+    'allow_comments_explain' => '',
+
+    'custom_css' => 'Свой CSS',
+    'custom_css_explain' => '',
+
+    'custom_js' => 'Свой JS',
+    'custom_js_explain' => '',
+
+    // messages
+    'updated' => 'Пост обновлен',
+    'created' => 'Пост создан',
+    'deleted' => 'Пост удален'
+
+);
