8aa446d5fc1cbd4be069982f3eed7f067994c4bf
artlancer <35227811+artlancer@users.noreply.github.com>
2018-01-15 21:03:53 +0500

    Create comments.php

---
+++ anchor/ru_RU/comments.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+
+    'comments' => 'Комментарии',
+    'nocomments_desc' => 'Нет комментариев.',
+    'editing_comment' => 'Редактировать комментарий',
+    'view_comment' => 'Показать комментарий',
+
+    // form fields
+    'name' => 'Имя',
+    'name_explain' => 'Автор',
+    'name_missing' => 'Пожалуйста, введите Имя',
+
+    'email' => 'Email',
+    'email_explain' => 'Email Автора',
+    'email_missing' => 'Пожалуйста, введите корректный email', // frontend message (appears on your site!)
+
+    'text' => 'Комментарий',
+    'text_explain' => '',
+    'text_missing' => 'Пожалуйста, напишите комментарий', // frontend message (appears on your site!)
+
+    'status' => 'Статус',
+    'status_explain' => '',
+
+    // messages
+    'created' => 'Ваш комментарий добавлен', // frontend message (appears on your site!)
+    'updated' => 'Ваш комментарий обновлен',
+    'deleted' => 'Ваш комментарий удален',
+
+    // email notification
+    'notify_subject' => 'Добавлен новый комментарий',
+    'nofity_heading' => 'Новый комментарий.'
+
+);
