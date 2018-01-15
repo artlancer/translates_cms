84c5691896638db1ef0072899115230ef3246c19
artlancer <35227811+artlancer@users.noreply.github.com>
2018-01-15 21:11:58 +0500

    Create pages.php

---
+++ anchor/ru_RU/pages.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+
+    'pages' => 'Страницы',
+
+    'create_page' => 'Создать страницу',
+    'nopages_desc' => 'У Вас нет страниц.',
+    'redirect' => 'Перенаправление(redirect)',
+
+    // form fields
+    'redirect_url' => 'На какой Url перенаправлять',
+    'redirect_missing' => 'Пожалуйста, введите корректный url',
+
+    'title' => 'Заголовок страницы',
+    'title_explain' => '',
+    'title_missing' => 'Заголовок страницы должен содержать минимум 3 символа',
+
+    'content' => 'Контент',
+    'content_explain' => 'Содержимое страницы. Используйте Markdown.',
+
+    'show_in_menu' => 'Показывать в Меню',
+    'show_in_menu_explain' => '',
+
+    'name' => 'Имя',
+    'name_explain' => '',
+
+    'slug' => 'ЧПУ и ID(Slug)',
+    'slug_explain' => 'Slug uri нужен для идентификации Вашей страницы, может содержать только ascii символы',
+    'slug_missing' => 'Slug должен содержать минимум 3 символа, используйте только ascii символы',
+    'slug_duplicate' => 'Такой Slug уже существует',
+    'slug_invalid' => 'Slug должен содержать буквы',
+
+    'status' => 'Статус',
+    'status_explain' => '',
+
+    'parent' => 'Родитель',
+    'parent_explain' => '',
+
+    'pagetype' => 'Тип Страницы',
+    'pagetype_explain' => 'Выберите тип страницы, который больше всего подходит для неё.',
+
+    // messages
+    'updated' => 'Cтраница обновлена.',
+    'created' => 'Cтраница создана.',
+    'deleted' => 'Cтраница удалена.'
+
+);
