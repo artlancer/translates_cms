7eef6f635c75f6a29a2a30b0a0a53950f12908ef
artlancer <35227811+artlancer@users.noreply.github.com>
2018-01-15 21:10:47 +0500

    Create metadata.php

---
+++ anchor/ru_RU/metadata.php
@@ -0,0 +1,50 @@
+<?php
+
+return array(
+
+    'metadata' => 'Настройки сайта',
+    'metadata_desc' => 'Настройте сайт',
+
+    'comment_settings' => 'Комментарии',
+    'theme_settings' => 'Внежний вид(шаблон)',
+
+    // form fields
+    'sitename' => 'Название сайта',
+    'sitename_explain' => '',
+    'sitename_missing' => 'Вашему сайту нужно Название!',
+
+    'sitedescription' => 'Описание сайта',
+    'sitedescription_explain' => 'Описание Вашего ссайта',
+    'sitedescription_missing' => 'Вашему сайту нужно Описание!',
+
+    'homepage' => 'Главная страница',
+    'homepage_explain' => '',
+
+    'postspage' => 'Страница с постами',
+    'postspage_explain' => '',
+
+    'dashboard_page' => 'Страница Админ Панели',
+    'dashboard_page_explain' => 'Страница Админ Панели по умолчанию',
+
+    'posts_per_page' => 'Постов на странице',
+    'posts_per_page_explain' => '',
+
+    'show_all_posts' => 'Показать все посты',
+    'show_all_posts_explain' => 'Показывать посты без пагинации (может работать медленно)',
+
+    'auto_publish_comments' => 'Автоматическая публикация комментариев',
+    'auto_publish_comments_explain' => '',
+
+    'comment_notifications' => 'Уведомление по email о новых комментариях',
+    'comment_notifications_explain' => '',
+
+    'comment_moderation_keys' => 'Ключевые слова для спам-фильтра',
+    'comment_moderation_keys_explain' => 'Перечислите ключевые слова для спам фильтра через запятую. Комментарии, содержащие эти слова автоматически будут отправлены в спам.',
+
+    'current_theme' => 'Текущая тема',
+    'current_theme_explain' => '',
+
+    // messages
+    'updated' => 'Метаданные обновлены',
+
+);
